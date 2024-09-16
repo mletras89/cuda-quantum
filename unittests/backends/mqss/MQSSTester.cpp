51f11f4e5fadfa63ab8539478060ecb4143dbff1
Martin Letras <di35hec@badwlrz-cl14642>
2024-09-16 12:21:36 +0000

    adding comments to test

--- unittests/backends/mqss/MQSSTester.cpp
+++ unittests/backends/mqss/MQSSTester.cpp
@@ -1,7 +1,34 @@
-/*******************************************************************************
- * Copyright (c) 2022 - 2024 NVIDIA Corporation & Affiliates.                  *
- * All rights reserved.                                                        *
- *                                                                             *
+/*-------------------------------------------------------------------------
+ This code and any associated documentation is provided "as is"
+
+ IN NO EVENT SHALL LEIBNIZ-RECHENZENTRUM (LRZ) BE LIABLE TO ANY PARTY FOR
+ DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES ARISING OUT
+ OF THE USE OF THIS CODE AND ITS DOCUMENTATION, EVEN IF LEIBNIZ-RECHENZENTRUM
+ (LRZ) HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ THE AFOREMENTIONED EXCLUSIONS OF LIABILITY DO NOT APPLY IN CASE OF INTENT
+ BY LEIBNIZ-RECHENZENTRUM (LRZ).
+
+ LEIBNIZ-RECHENZENTRUM (LRZ), SPECIFICALLY DISCLAIMS ANY WARRANTIES, INCLUDING,
+ BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE.
+
+ THE CODE PROVIDED HEREUNDER IS ON AN "AS IS" BASIS, LEIBNIZ-RECHENZENTRUM (LRZ)
+ HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR
+ MODIFICATIONS.
+ -------------------------------------------------------------------------
+
+  @author Martin Letras
+  @date   September 2024
+  @version 1.0
+  @ brief
+	Test to verify the connection of CudaQ and the MQSS. The test is mainly
+	based on Quantinuum test provided by Nvidia.
+	When executing the cudaq::sample(kernel), the runtime manager of CudaQ
+	sends a job containing the quake code that has to be processed by the
+	MQSS. Once the job is done, MQSS shall return the results. The
+	cudaq::sample returns the results.
+
+ *******************************************************************************
  * This source code and the accompanying materials are made available under    *
  * the terms of the Apache License 2.0 which accompanies this distribution.    *
  ******************************************************************************/
@@ -43,379 +70,6 @@ CUDAQ_TEST(MQSSTester, checkSampleSync) {
   counts.dump();
   EXPECT_EQ(counts.size(), 2);
 }
-
-//CUDAQ_TEST(MQSSTester, checkSampleSyncEmulate) {
-//  std::string home = std::getenv("HOME");
-//  std::string fileName = home + "/FakeCppMQSS.config";
-//  auto backendString =
-//      fmt::format(fmt::runtime(backendStringTemplate), mockPort, fileName);
-//  backendString =
-//      std::regex_replace(backendString, std::regex("false"), "true");
-//
-//  auto &platform = cudaq::get_platform();
-//  platform.setTargetBackend(backendString);
-//
-//  auto kernel = cudaq::make_kernel();
-//  auto qubit = kernel.qalloc(2);
-//  kernel.h(qubit[0]);
-//  kernel.x<cudaq::ctrl>(qubit[0], qubit[1]);
-//  kernel.mz(qubit[0]);
-//  kernel.mz(qubit[1]);
-//
-//  auto counts = cudaq::sample(kernel);
-//  counts.dump();
-//  EXPECT_EQ(counts.size(), 2);
-//}
-//
-//CUDAQ_TEST(MQSSTester, checkSampleAsync) {
-//  std::string home = std::getenv("HOME");
-//  std::string fileName = home + "/FakeCppMQSS.config";
-//  auto backendString =
-//      fmt::format(fmt::runtime(backendStringTemplate), mockPort, fileName);
-//
-//  auto &platform = cudaq::get_platform();
-//  platform.setTargetBackend(backendString);
-//
-//  auto kernel = cudaq::make_kernel();
-//  auto qubit = kernel.qalloc(2);
-//  kernel.h(qubit[0]);
-//  kernel.mz(qubit[0]);
-//
-//  auto future = cudaq::sample_async(kernel);
-//  auto counts = future.get();
-//  EXPECT_EQ(counts.size(), 2);
-//}
-
-//CUDAQ_TEST(MQSSTester, checkSampleAsyncEmulate) {
-//  std::string home = std::getenv("HOME");
-//  std::string fileName = home + "/FakeCppMQSS.config";
-//  auto backendString =
-//      fmt::format(fmt::runtime(backendStringTemplate), mockPort, fileName);
-//  backendString =
-//      std::regex_replace(backendString, std::regex("false"), "true");
-//
-//  auto &platform = cudaq::get_platform();
-//  platform.setTargetBackend(backendString);
-//
-//  auto kernel = cudaq::make_kernel();
-//  auto qubit = kernel.qalloc(2);
-//  kernel.h(qubit[0]);
-//  kernel.mz(qubit[0]);
-//
-//  auto future = cudaq::sample_async(kernel);
-//  auto counts = future.get();
-//  counts.dump();
-//  EXPECT_EQ(counts.size(), 2);
-//}
-//
-//CUDAQ_TEST(MQSSTester, checkSampleAsyncLoadFromFile) {
-//  std::string home = std::getenv("HOME");
-//  std::string fileName = home + "/FakeCppMQSS.config";
-//  auto backendString =
-//      fmt::format(fmt::runtime(backendStringTemplate), mockPort, fileName);
-//
-//  auto &platform = cudaq::get_platform();
-//  platform.setTargetBackend(backendString);
-//
-//  auto kernel = cudaq::make_kernel();
-//  auto qubit = kernel.qalloc(2);
-//  kernel.h(qubit[0]);
-//  kernel.mz(qubit[0]);
-//
-//  // Can sample asynchronously and get a future
-//  auto future = cudaq::sample_async(kernel);
-//
-//  // Future can be persisted for later
-//  {
-//    std::ofstream out("saveMe.json");
-//    out << future;
-//  }
-//
-//  // Later you can come back and read it in
-//  cudaq::async_result<cudaq::sample_result> readIn;
-//  std::ifstream in("saveMe.json");
-//  in >> readIn;
-//
-//  // Get the results of the read in future.
-//  auto counts = readIn.get();
-//  EXPECT_EQ(counts.size(), 2);
-//
-//  std::remove("saveMe.json");
-//}
-//
-//CUDAQ_TEST(MQSSTester, checkObserveSync) {
-//  std::string home = std::getenv("HOME");
-//  std::string fileName = home + "/FakeCppMQSS.config";
-//  auto backendString =
-//      fmt::format(fmt::runtime(backendStringTemplate), mockPort, fileName);
-//
-//  auto &platform = cudaq::get_platform();
-//  platform.setTargetBackend(backendString);
-//
-//  auto [kernel, theta] = cudaq::make_kernel<double>();
-//  auto qubit = kernel.qalloc(2);
-//  kernel.x(qubit[0]);
-//  kernel.ry(theta, qubit[1]);
-//  kernel.x<cudaq::ctrl>(qubit[1], qubit[0]);
-//
-//  using namespace cudaq::spin;
-//  cudaq::spin_op h = 5.907 - 2.1433 * x(0) * x(1) - 2.1433 * y(0) * y(1) +
-//                     .21829 * z(0) - 6.125 * z(1);
-//  auto result = cudaq::observe(10000, kernel, h, .59);
-//  result.dump();
-//
-//  printf("ENERGY: %lf\n", result.expectation());
-//  EXPECT_TRUE(isValidExpVal(result.expectation()));
-//}
-//
-//CUDAQ_TEST(MQSSTester, checkObserveSyncEmulate) {
-//  std::string home = std::getenv("HOME");
-//  std::string fileName = home + "/FakeCppMQSS.config";
-//  auto backendString =
-//      fmt::format(fmt::runtime(backendStringTemplate), mockPort, fileName);
-//  backendString =
-//      std::regex_replace(backendString, std::regex("false"), "true");
-//
-//  auto &platform = cudaq::get_platform();
-//  platform.setTargetBackend(backendString);
-//
-//  auto [kernel, theta] = cudaq::make_kernel<double>();
-//  auto qubit = kernel.qalloc(2);
-//  kernel.x(qubit[0]);
-//  kernel.ry(theta, qubit[1]);
-//  kernel.x<cudaq::ctrl>(qubit[1], qubit[0]);
-//
-//  using namespace cudaq::spin;
-//  cudaq::spin_op h = 5.907 - 2.1433 * x(0) * x(1) - 2.1433 * y(0) * y(1) +
-//                     .21829 * z(0) - 6.125 * z(1);
-//  auto result = cudaq::observe(100000, kernel, h, .59);
-//  result.dump();
-//
-//  printf("ENERGY: %lf\n", result.expectation());
-//  EXPECT_TRUE(isValidExpVal(result.expectation()));
-//}
-//
-//CUDAQ_TEST(MQSSTester, checkObserveAsync) {
-//  std::string home = std::getenv("HOME");
-//  std::string fileName = home + "/FakeCppMQSS.config";
-//  auto backendString =
-//      fmt::format(fmt::runtime(backendStringTemplate), mockPort, fileName);
-//
-//  auto &platform = cudaq::get_platform();
-//  platform.setTargetBackend(backendString);
-//
-//  auto [kernel, theta] = cudaq::make_kernel<double>();
-//  auto qubit = kernel.qalloc(2);
-//  kernel.x(qubit[0]);
-//  kernel.ry(theta, qubit[1]);
-//  kernel.x<cudaq::ctrl>(qubit[1], qubit[0]);
-//
-//  using namespace cudaq::spin;
-//  cudaq::spin_op h = 5.907 - 2.1433 * x(0) * x(1) - 2.1433 * y(0) * y(1) +
-//                     .21829 * z(0) - 6.125 * z(1);
-//  auto future = cudaq::observe_async(kernel, h, .59);
-//
-//  auto result = future.get();
-//  result.dump();
-//
-//  printf("ENERGY: %lf\n", result.expectation());
-//  EXPECT_TRUE(isValidExpVal(result.expectation()));
-//}
-//
-//CUDAQ_TEST(MQSSTester, checkObserveAsyncEmulate) {
-//  std::string home = std::getenv("HOME");
-//  std::string fileName = home + "/FakeCppMQSS.config";
-//  auto backendString =
-//      fmt::format(fmt::runtime(backendStringTemplate), mockPort, fileName);
-//  backendString =
-//      std::regex_replace(backendString, std::regex("false"), "true");
-//
-//  auto &platform = cudaq::get_platform();
-//  platform.setTargetBackend(backendString);
-//
-//  auto [kernel, theta] = cudaq::make_kernel<double>();
-//  auto qubit = kernel.qalloc(2);
-//  kernel.x(qubit[0]);
-//  kernel.ry(theta, qubit[1]);
-//  kernel.x<cudaq::ctrl>(qubit[1], qubit[0]);
-//
-//  using namespace cudaq::spin;
-//  cudaq::spin_op h = 5.907 - 2.1433 * x(0) * x(1) - 2.1433 * y(0) * y(1) +
-//                     .21829 * z(0) - 6.125 * z(1);
-//  auto future = cudaq::observe_async(100000, 0, kernel, h, .59);
-//
-//  auto result = future.get();
-//  result.dump();
-//
-//  printf("ENERGY: %lf\n", result.expectation());
-//  EXPECT_TRUE(isValidExpVal(result.expectation()));
-//}
-//
-//CUDAQ_TEST(MQSSTester, checkObserveAsyncLoadFromFile) {
-//
-//  std::string home = std::getenv("HOME");
-//  std::string fileName = home + "/FakeCppMQSS.config";
-//  auto backendString =
-//      fmt::format(fmt::runtime(backendStringTemplate), mockPort, fileName);
-//
-//  auto &platform = cudaq::get_platform();
-//  platform.setTargetBackend(backendString);
-//
-//  auto [kernel, theta] = cudaq::make_kernel<double>();
-//  auto qubit = kernel.qalloc(2);
-//  kernel.x(qubit[0]);
-//  kernel.ry(theta, qubit[1]);
-//  kernel.x<cudaq::ctrl>(qubit[1], qubit[0]);
-//
-//  using namespace cudaq::spin;
-//  cudaq::spin_op h = 5.907 - 2.1433 * x(0) * x(1) - 2.1433 * y(0) * y(1) +
-//                     .21829 * z(0) - 6.125 * z(1);
-//  auto future = cudaq::observe_async(kernel, h, .59);
-//
-//  {
-//    std::ofstream out("saveMeObserve.json");
-//    out << future;
-//  }
-//
-//  // Later you can come back and read it in
-//  cudaq::async_result<cudaq::observe_result> readIn(&h);
-//  std::ifstream in("saveMeObserve.json");
-//  in >> readIn;
-//
-//  // Get the results of the read in future.
-//  auto result = readIn.get();
-//
-//  std::remove("saveMeObserve.json");
-//  result.dump();
-//
-//  printf("ENERGY: %lf\n", result.expectation());
-//  EXPECT_TRUE(isValidExpVal(result.expectation()));
-//}
-//
-//CUDAQ_TEST(MQSSTester, checkControlledRotations) {
-//  // Checks for more advanced controlled rotations that only
-//  // work in emulation.
-//  std::string home = std::getenv("HOME");
-//  std::string fileName = home + "/FakeCppMQSS.config";
-//  auto backendString =
-//      fmt::format(fmt::runtime(backendStringTemplate), mockPort, fileName);
-//
-//  auto &platform = cudaq::get_platform();
-//  platform.setTargetBackend(backendString);
-//
-//  // rx: pi
-//  {
-//    auto kernel = cudaq::make_kernel();
-//    auto controls1 = kernel.qalloc(2);
-//    auto controls2 = kernel.qalloc(2);
-//    auto control3 = kernel.qalloc();
-//    auto target = kernel.qalloc();
-//
-//    // All of our controls in the 1-state.
-//    kernel.x(controls1);
-//    kernel.x(controls2);
-//    kernel.x(control3);
-//
-//    kernel.rx<cudaq::ctrl>(M_PI, controls1, controls2, control3, target);
-//
-//    std::cout << kernel.to_quake() << "\n";
-//
-//    auto counts = cudaq::sample(kernel);
-//    counts.dump();
-//
-//    // Target qubit should've been rotated to |1>.
-//    EXPECT_EQ(counts.count("0000111111"), 1000);
-//  }
-//
-//  // rx: 0.0
-//  {
-//    auto kernel = cudaq::make_kernel();
-//    auto controls1 = kernel.qalloc(2);
-//    auto controls2 = kernel.qalloc(2);
-//    auto control3 = kernel.qalloc();
-//    auto target = kernel.qalloc();
-//
-//    // All of our controls in the 1-state.
-//    kernel.x(controls1);
-//    kernel.x(controls2);
-//    kernel.x(control3);
-//
-//    kernel.rx<cudaq::ctrl>(0.0, controls1, controls2, control3, target);
-//
-//    auto counts = cudaq::sample(kernel);
-//    counts.dump();
-//
-//    // Target qubit should've stayed in |0>
-//    EXPECT_EQ(counts.count("0000111110"), 1000);
-//  }
-//
-//  // ry: pi
-//  {
-//    auto kernel = cudaq::make_kernel();
-//    auto controls1 = kernel.qalloc(2);
-//    auto controls2 = kernel.qalloc(2);
-//    auto control3 = kernel.qalloc();
-//    auto target = kernel.qalloc();
-//
-//    // All of our controls in the 1-state.
-//    kernel.x(controls1);
-//    kernel.x(controls2);
-//    kernel.x(control3);
-//
-//    kernel.ry<cudaq::ctrl>(M_PI, controls1, controls2, control3, target);
-//
-//    auto counts = cudaq::sample(kernel);
-//    counts.dump();
-//
-//    // Target qubit should've been rotated to |1>
-//    EXPECT_EQ(counts.count("0000111111"), 1000);
-//  }
-//
-//  // ry: pi / 2
-//  {
-//    cudaq::set_random_seed(4);
-//
-//    auto kernel = cudaq::make_kernel();
-//    auto controls1 = kernel.qalloc(2);
-//    auto controls2 = kernel.qalloc(2);
-//    auto control3 = kernel.qalloc();
-//    auto target = kernel.qalloc();
-//
-//    // All of our controls in the 1-state.
-//    kernel.x(controls1);
-//    kernel.x(controls2);
-//    kernel.x(control3);
-//
-//    kernel.ry<cudaq::ctrl>(M_PI_2, controls1, controls2, control3, target);
-//
-//    auto counts = cudaq::sample(kernel);
-//    counts.dump();
-//
-//    // Target qubit should have a 50/50 mix between |0> and |1>
-//    EXPECT_TRUE(counts.count("0000111111") < 550);
-//    EXPECT_TRUE(counts.count("0000111110") > 450);
-//  }
-//
-//  {
-//    auto kernel = cudaq::make_kernel();
-//    auto controls1 = kernel.qalloc(3);
-//    auto controls2 = kernel.qalloc(3);
-//    auto control3 = kernel.qalloc();
-//    auto target = kernel.qalloc();
-//
-//    kernel.x(controls1);
-//    kernel.x(control3);
-//    // Should do nothing.
-//    kernel.x<cudaq::ctrl>(controls1, controls2, control3, target);
-//    kernel.x(controls2);
-//    // Should rotate `target`.
-//    kernel.rx<cudaq::ctrl>(M_PI, controls1, controls2, control3, target);
-//
-//    auto counts = cudaq::sample(kernel);
-//    counts.dump();
-//    EXPECT_EQ(counts.count("00000011111111"), 1000);
-//  }
-//}
 
 int main(int argc, char **argv) {
   std::string home = std::getenv("HOME");
