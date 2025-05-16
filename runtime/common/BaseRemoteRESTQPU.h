7441ddf761be0c7a946c74d4d6063c3f2072e473
Martin Letras <di35hec@badwlrz-cl14642>
2025-05-16 17:05:59 +0000

    More work on consoloditating branches

--- runtime/common/BaseRemoteRESTQPU.h
+++ runtime/common/BaseRemoteRESTQPU.h
@@ -295,11 +295,6 @@ public:
         postCodeGenPasses = config.BackendConfig->PostCodeGenPasses;
       }
     }
-    /*std::cout << "LETRAS: codegenTranslation:" << codegenTranslation << std::endl;
-    if (codegenTranslation == "MQSS"){
-	std::cout << "LETRAS: sending code MQSS" << std::endl;
-    }*/
-	
     std::string allowEarlyExitSetting =
         (codegenTranslation == "qir-adaptive") ? "1" : "0";
 
@@ -385,7 +380,6 @@ public:
         }
       }
     }
-    //std::cout << "Letras: output_names "  << output_names  <<std::endl;
     return output_names;
   }
 
@@ -600,21 +594,7 @@ public:
         runPassPipeline("func.func(combine-measurements)", module);
 
     // Get the code gen translation
-    //std::cout << "LETRAS: codegenTranslation " << codegenTranslation << std::endl;
-    // Quake code before lowering it to QIR or QASM
-    //std::cout << "LETRAS: code before lowering it to QIR or QASM" << std::endl;
-    for (auto &[name, moduleOpI] : modules){
-      std::string codeStri;
-      llvm::raw_string_ostream outStr(codeStri);
-        if (disableMLIRthreading)
-          moduleOpI.getContext()->disableMultithreading();
-        //std::cout << "LETRAS: module name " << name << std::endl;
-        //std::cout << "LETRAS: module " << std::endl;
-        //moduleOpI->dump();
-    }
     auto translation = cudaq::getTranslation(codegenTranslation);
-    //std::string tName = std::string(translation.getDescription());
-    //std::cout << "LETRAS: Translation " << tName << std::endl;
 
     // Apply user-specified codegen
     std::vector<cudaq::KernelExecution> codes;
